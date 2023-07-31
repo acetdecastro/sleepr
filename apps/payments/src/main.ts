1a30c474b3e06b16aecdc847c3ab48f07fba1724
Ace De Castro <acetdecastro@gmail.com>
2023-07-31 22:42:51 +0800

    chore: add createCharge route in payments service

--- apps/payments/src/main.ts
+++ apps/payments/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { PaymentsModule } from './payments.module';
+import { Transport } from '@nestjs/microservices';
+import { Logger as PinoLogger } from 'nestjs-pino';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(PaymentsModule);
-  await app.listen(3000);
+  app.useLogger(app.get(PinoLogger)); // as reservations app logger
+  const configService = app.get(ConfigService);
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0', // bind all available IP addresses on the machine
+      port: configService.get('TCP_PORT'),
+    },
+  });
+  await app.startAllMicroservices();
 }
 bootstrap();
--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,8 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import Stripe from 'stripe';
+import { CreateChargeDto } from './dto/create-charge.dto';
 
 @Injectable()
 export class PaymentsService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly stripe = new Stripe(
+    this.configService.get('STRIPE_API_KEY'),
+    {
+      apiVersion: '2022-11-15',
+    },
+  );
+
+  constructor(private readonly configService: ConfigService) {}
+
+  async createCharge({ card, amount }: CreateChargeDto) {
+    try {
+      const paymentMethod = await this.stripe.paymentMethods.create({
+        type: 'card',
+        card,
+      });
+
+      const paymentIntent = await this.stripe.paymentIntents.create({
+        payment_method: paymentMethod.id,
+        amount: amount * 100,
+        confirm: true,
+        payment_method_types: ['card'],
+        currency: 'usd',
+      });
+
+      return paymentIntent;
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
   }
 }
