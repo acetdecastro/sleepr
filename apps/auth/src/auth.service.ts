8833e43132a515ddcffbd407c0c2eef1beb1f312
Ace De Castro <acetdecastro@gmail.com>
2023-08-03 18:16:13 +0800

    refactor: move user-schema to common/libs/models

--- apps/auth/src/auth.service.ts
+++ apps/auth/src/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { UserDocument } from './users/models/user.schema';
+import { UserDocument } from '@app/common';
 import { Response } from 'express';
 import { ConfigService } from '@nestjs/config';
 import { TokenPayload } from './interfaces/token-payload.interface';
--- apps/auth/src/users/users.repository.ts
+++ apps/auth/src/users/users.repository.ts
@@ -1,6 +1,9 @@
-import { AbstractRepository } from '@app/common';
+import {
+  AbstractRepository,
+  UserDocument,
+  usersCollectionName,
+} from '@app/common';
 import { Injectable, Logger } from '@nestjs/common';
-import { UserDocument, usersCollectionName } from './models/user.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
