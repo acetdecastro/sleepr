09349c77bd51e7ac8704bce78da8b755b29ad609
Ace De Castro <acetdecastro@gmail.com>
2023-08-14 11:55:29 +0800

    feat: healthcheck module

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -3,7 +3,6 @@ import {
   Inject,
   Injectable,
   InternalServerErrorException,
-  Logger,
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { ClientProxy } from '@nestjs/microservices';
@@ -18,8 +17,6 @@ export class PaymentsService {
       apiVersion: '2022-11-15',
     },
   );
-
-  private readonly logger = new Logger(PaymentsService.name);
 
   constructor(
     private readonly configService: ConfigService,
